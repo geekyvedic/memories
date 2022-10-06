296069adb6025e154e8fa41e8c0625a9ab833571
Vedic <vedic603@gmail.com>
2022-10-06 12:51:57 +0530

    init commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Memories
+
+![Memories](./screenshots/screenshot.png)
+
+A simple social media app that allows users to post interesting events that happened in their lives.
---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const postRoutes = require("./routes/posts");
+
+const app = express();
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors());
+
+app.use('/posts', postRoutes);
+
+const CONNECTION_URL = 'mongodb+srv://vedicsince2000:kanipakam99@cluster0.kiu8dtd.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT|| 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
+mongoose.set('useFindAndModify', false);
